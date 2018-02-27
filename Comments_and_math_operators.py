379cddfd84300565418d551cf9b95d70f8444ee8
Adrian Amal Moran <amaliamoran@gmail.com>
2018-02-26 20:13:01 -0500

    Work for Python for Absolute Beginners
    Work for Python for Absoluyte Beginners

---
+++ Comments_and_math_operators.py
@@ -0,0 +1,25 @@
+#
+
+add = 5+5
+sub = 5-5
+mult = 5*5
+div = 5/5
+power = 5 ** 5
+mod = 15%5
+
+print(power)
+
+#
+
+mod1 = 7%5
+mod2 = 16%6
+mod3 = 4%3
+print(mod1)
+print(mod2)
+print(mod3)
+#
+
+ordOp1 = 7+6+9-4*((9-2)**2)/7
+print(ordOp1)
+ordOp2 = (6%4*(7+(7+2)*3))**2
+print(ordOp2)
---
+++ DataTypes.py
@@ -0,0 +1,8 @@
+intval = 0
+floatval = 2.5
+boolval= True
+print(boolval)
+boolval= False
+print(boolval)
+print(intval)
+print(floatval)
